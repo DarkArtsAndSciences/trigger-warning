f48230b37d8e53267526d884fd71039d9ba4b43d
User Name <user@server>
2013-09-06 01:16:34 -0700

    Added repr() to args printed from log_function_call(). Strings are now quoted.

--- utils.py
+++ utils.py
@@ -9,6 +9,6 @@ def log_function_call(message=""):
 	caller_frame = inspect.stack()[1]
 	function_name = caller_frame[3]
 	args, vargs, kwargs, values = inspect.getargvalues(caller_frame[0])
-	pretty_args = ', '.join(['{}={}'.format(arg, values[arg]) for arg in args])
+	pretty_args = ', '.join(['{}={}'.format(arg, repr(values[arg])) for arg in args])
 	timestamp = datetime.datetime.now()
 	print '{}: {}({}) {}'.format(timestamp, function_name, pretty_args, message)
