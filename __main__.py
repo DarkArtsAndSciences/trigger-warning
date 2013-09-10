ea5c96cce386423079e7ea49a1d5b09c943c0048
User Name <user@server>
2013-09-10 10:12:28 -0700

    Turned off profiling.

--- __main__.py
+++ __main__.py
@@ -4,7 +4,7 @@ import state_manager
 import time_manager
 import trigger_manager
 
-import cProfile as profile
+#import cProfile as profile
 
 state_manager.init()
 time_manager.init()
@@ -12,5 +12,5 @@ trigger_manager.init()
 interface_manager.init()
 boids.init()
 
-#interface_manager.loop()
-profile.run('interface_manager.loop()', sort='cumulative')
+interface_manager.loop()
+#profile.run('interface_manager.loop()', sort='cumulative')
